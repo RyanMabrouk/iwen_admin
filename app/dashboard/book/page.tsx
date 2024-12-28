9f9a0f81de1b70124bd9649a4a6a2c2bdbab0248
MontassarSN <snenmontassar@gmail.com>
2024-12-28 22:47:12 +0100

    Enhance book page component: update breadcrumb and title based on bookId from search parameters

--- app/dashboard/book/page.tsx
+++ app/dashboard/book/page.tsx
@@ -1,22 +1,29 @@
+'use client';
 import React from 'react';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import Form from './ui/form';
-
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const bookId = searchParams.get('bookId');
+  
   const breadcrumbItems = [
     { title: 'إحصائيات', link: '/dashboard' },
-    { title: 'تعديل كتاب', link: '/dashboard/editBook' }
+    { title: bookId ? 'تعديل' : 'إنشاء كتاب جديد', link: `/dashboard/book` }
   ];
+  
+  const pageTitle = bookId ? 'تعديل الكتاب' : 'إنشاء كتاب جديد';
+
   return (
     <PageContainer>
       <div
-        dir='rtl'
-        className="mx-auto   w-full max-w-[50rem] border bg-white p-5 shadow-md md:p-10 "
+        dir="rtl"
+        className="mx-auto w-full max-w-[50rem] border bg-white p-5 shadow-md md:p-10"
       >
-        <Breadcrumbs items={breadcrumbItems}/>
-        <h1 className="mb-4 text-2xl font-bold mt-5 "  >تعديل الكتاب</h1>
+        <Breadcrumbs items={breadcrumbItems} />
+        <h1 className="mb-4 mt-5 text-2xl font-bold">{pageTitle}</h1>
         <Form />
       </div>
     </PageContainer>
